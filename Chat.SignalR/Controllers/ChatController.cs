5a9e3ff41f63cac4534cad6e6b2762a8e0482ba6
vmetal123 <bladi22@yahoo.com>
2019-09-17 21:52:22 -0600

    Agregar archivos de proyecto.

---
+++ Chat.SignalR/Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chat.SignalR.Hubs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.SignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> hubContext;
+        public ChatController(IHubContext<ChatHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+        
+        public void SendMessage(string user, string message)
+        {
+            hubContext.Clients.All.SendAsync("ReceivedMessage", user, message);
+        }
+    }
+}
